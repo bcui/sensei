62c2ad04cc742c7b04097c6b5807bd0f38105d5a
Baoqiu Cui <bcui@linkedin.com>
2011-11-01 14:29:21 -0700

    Modified test case to cover facet init params.

--- clients/python/test/test_ucp_template.py
+++ clients/python/test/test_ucp_template.py
@@ -13,7 +13,7 @@ class TestUCPQueryTemplate(unittest.TestCase):
 
   def testBasics(self):
     stmt = \
-        "SELECT color, year FROM cars WHERE QUERY IS 'cool $myKeywords' AND year < 1996 ORDER BY color GROUP BY color TOP 15;"
+        "SELECT color, year FROM cars WHERE QUERY IS 'cool $myKeywords' AND year < 1996 GIVEN FACET PARAM (My-Network, 'srcid', int, '$memberId'), (My-Network, 'metaData', string, 'myMetaData') ORDER BY color GROUP BY color TOP 15;"
 
     info = {
       "name": "nus_member",
@@ -24,11 +24,39 @@ class TestUCPQueryTemplate(unittest.TestCase):
     self.assertEqual(json.dumps(parser_agent.construct_ucp_json(info),
                                 sort_keys=True, indent=4),
                      """{
-    "bql": "SELECT color, year FROM cars WHERE QUERY IS 'cool $myKeywords' AND year < 1996 ORDER BY color GROUP BY color TOP 15;", 
+    "bql": "SELECT color, year FROM cars WHERE QUERY IS 'cool $myKeywords' AND year < 1996 GIVEN FACET PARAM (My-Network, 'srcid', int, '$memberId'), (My-Network, 'metaData', string, 'myMetaData') ORDER BY color GROUP BY color TOP 15;", 
     "description": "Test BQL query template generator", 
+    "facets": {
+        "array": [
+            {
+                "initParams": {
+                    "array": [
+                        {
+                            "name": "srcid", 
+                            "type": "INT", 
+                            "values": [
+                                "$memberId"
+                            ]
+                        }, 
+                        {
+                            "name": "metaData", 
+                            "type": "STRING", 
+                            "values": [
+                                "myMetaData"
+                            ]
+                        }
+                    ]
+                }, 
+                "name": "My-Network"
+            }
+        ]
+    }, 
     "feedQuery": {
         "auxParams": {
             "array": [
+                {
+                    "name": "memberId"
+                }, 
                 {
                     "name": "myKeywords"
                 }
